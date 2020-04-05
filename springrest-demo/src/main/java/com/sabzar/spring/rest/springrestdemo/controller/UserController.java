ff9b52d2043fea5fa878fc73083dd3eedde2db9b
e230819 <sabzar.rangraze@evry.com>
2020-04-05 22:49:26 +0530

    added log

--- springrest-demo/src/main/java/com/sabzar/spring/rest/springrestdemo/controller/UserController.java
+++ springrest-demo/src/main/java/com/sabzar/spring/rest/springrestdemo/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.sabzar.spring.rest.springrestdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping("greeting")
     public String greeting(){
-        return "Welcome to the DevOPP ";
+        log.info("*** User Controller get greating");
+        return "Welcome to the DevOPP";
     }
 }
--- springrest-demo/src/test/java/com/sabzar/spring/rest/springrestdemo/controller/UserControllerTest.java
+++ springrest-demo/src/test/java/com/sabzar/spring/rest/springrestdemo/controller/UserControllerTest.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -18,6 +20,8 @@ class UserControllerTest {
     @Test
     void greeting() throws  Exception{
 
-        mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk());
+       mockMvc.perform(get("/greeting"))
+                .andDo(print())
+                .andExpect(status().isOk()).andExpect(content().string(containsString("Welcome to the DevOPP")));
     }
 }
